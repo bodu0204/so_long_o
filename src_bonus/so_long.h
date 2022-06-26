e038dbae5aa587e676da687a59e140a2b007e8fd
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-06-26 18:40:11 +0900

    commit form makefile

--- src_bonus/so_long.h
+++ src_bonus/so_long.h
@@ -1,10 +1,10 @@
 #ifndef SO_LONG_H
-#define SO_LONG_H
+# define SO_LONG_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include "../minilibx_mms_20200219/mlx.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "../minilibx_mms_20200219/mlx.h"
 
 typedef struct s_mob
 {
@@ -43,55 +43,52 @@ typedef struct s_info
 	u_int8_t	buf[33];
 }	t_info;
 
+# define BUFSIZ 64
+# define BLOCKLEN 30
+# define WEIFHT 0x7ff
+# define DEATHLEN 10
+# define MAMLEN 30
+# define DADLEN 150
+# define TURN (WEIFHT / BLOCKLEN)
 
+# define FILE_0b "img/floor_bonus_big.xpm"
+# define FILE_1b "img/wall_big.xpm"
+# define FILE_0 "img/floor_bonus.xpm"
+# define FILE_1 "img/wall.xpm"
+# define FILE_Pf "img/anya3.xpm"
+# define FILE_Pb "img/anya0.xpm"
+# define FILE_Pl "img/anya1.xpm"
+# define FILE_Pr "img/anya2.xpm"
+# define FILE_C "img/peanuts.xpm"
+# define FILE_E "img/tv.xpm"
+# define FILE_M "img/mam.xpm"
+# define FILE_F "img/dad.xpm"
+# define FILE_W01 "img/book001.xpm"
+# define FILE_W02 "img/book003.xpm"
+# define FILE_W03 "img/book005.xpm"
+# define FILE_W04 "img/book010.xpm"
+# define FILE_W05 "img/book020.xpm"
+# define FILE_W06 "img/book030.xpm"
+# define FILE_W07 "img/book050.xpm"
+# define FILE_W08 "img/book080.xpm"
+# define FILE_W09 "img/book100.xpm"
+# define FILE_W10 "img/book999.xpm"
+# define FILE_HAPPY "img/happy_end.xpm"
+# define FILE_BAD "img/bad_end.xpm"
 
+# define FRONT 0
+# define RIGHT 1
+# define BACK 2
+# define LEFT 3
+# define ESC_KEY 4
+# define NO_KEY 5
+# define DEAD 6
 
-#define BUFSIZ 64
-#define BLOCKLEN 30
-#define WEIFHT 0x7ff
-#define DEATHLEN 10
-#define MAMLEN 30
-#define DADLEN 150
-#define TURN (WEIFHT / BLOCKLEN)
-
-#define FILE_0b		"img/floor_bonus_big.xpm"
-#define FILE_1b		"img/wall_big.xpm"
-#define FILE_0		"img/floor_bonus.xpm"
-#define FILE_1		"img/wall.xpm"
-#define FILE_Pf		"img/anya3.xpm"
-#define FILE_Pb		"img/anya0.xpm"
-#define FILE_Pl		"img/anya1.xpm"
-#define FILE_Pr		"img/anya2.xpm"
-#define FILE_C		"img/peanuts.xpm"
-#define FILE_E		"img/tv.xpm"
-#define FILE_M		"img/mam.xpm"
-#define FILE_F		"img/dad.xpm"
-#define FILE_W01	"img/book001.xpm"
-#define FILE_W02	"img/book003.xpm"
-#define FILE_W03	"img/book005.xpm"
-#define FILE_W04	"img/book010.xpm"
-#define FILE_W05	"img/book020.xpm"
-#define FILE_W06	"img/book030.xpm"
-#define FILE_W07	"img/book050.xpm"
-#define FILE_W08	"img/book080.xpm"
-#define FILE_W09	"img/book100.xpm"
-#define FILE_W10	"img/book999.xpm"
-#define FILE_HAPPY	"img/happy_end.xpm"
-#define FILE_BAD	"img/bad_end.xpm"
-
-#define FRONT 0
-#define RIGHT 1
-#define BACK 2
-#define LEFT 3
-#define ESC_KEY 4
-#define NO_KEY 5
-#define DEAD 6
-
-#define START 0
-#define GAME 1
-#define SUCCESS 2
-#define FALE 3
-#define END 4
+# define START 0
+# define GAME 1
+# define SUCCESS 2
+# define FALE 3
+# define END 4
 
 
 
